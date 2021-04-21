4d82421871d42ef4d9d518cc479daf8a240d2114
advayramprasad <75668728+advayramprasad@users.noreply.github.com>
2021-04-21 21:24:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import WelcomeScreen from './screens/WelcomeScreen';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {AppDrawerNavigator} from './components/AppDrawerNavigator';
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen: {
+    screen: WelcomeScreen
+  },
+  Drawer:{
+    screen: AppDrawerNavigator
+  },
+  BottomTab: {
+    screen: AppDrawerNavigator
+  }
+})
+
+const AppContainer = createAppContainer(switchNavigator);
